77e358f88eab0e8ae2eddb8899444600d1f49616
wtingting <1101559421@qq.com>
2016-08-10 14:08:14 +0800

    修改经销商

--- WebPages/distributor/js/my-company-main.js
+++ WebPages/distributor/js/my-company-main.js
@@ -1,93 +1,69 @@
-    function suppermarketactivitylist(container) {
-        this.container = $(container);
-        this.companytemplate = [
-       '<div class="m-company-top">',
-            '<div class="userimg"><img src="{@if distributor_pic ==""}../image/cs04.jpg {@else}${distributor_pic}{@/if}" /></div>',
-            '<div class="userinfo">',
-                '<div class="zi">',
-                    '<span>${distributorname}</span><br />',
-                    '${address}<br />',
-                    '${mobilephone}',
-                '</div>',
-                '<div class="ewm">',
-                    '<a href="m-company-ewm.html"><img id="qrcode" orgsrc="/webapi/consumer/weixin/register_generate_code?qrtype=20&combinetext=0&combineicon=0"/></a>',
-                '</div>',
+function suppermarketactivitylist(container) {
+    this.container = $(container);
+    this.companytemplate = [
+   '<div class="m-company-top">',
+        '<div class="userimg"><img src="{@if distributor_pic ==""}../image/cs04.jpg {@else}${distributor_pic}{@/if}" /></div>',
+        '<div class="userinfo">',
+            '<div class="zi">',
+                '<span>${distributorname}</span><br />',
+                '${address}<br />',
+                '${mobilephone}',
+            '</div>',
+            '<div class="ewm">',
+                '<a href="m-company-ewm.html"><img id="qrcode" orgsrc="/webapi/consumer/weixin/register_generate_code?qrtype=20&combinetext=0&combineicon=0"/></a>',
             '</div>',
         '</div>',
-        '<div class="m-company-listbox-w">',
-            '<div class="m-company-listbox1-w">',
-                '<div class="m-company-listbox1">',
-                    '<div class="listbox-zi">',
-                        '${invitedretailercount}<br />邀请店铺',
-                    '</div>',
-                '</div>',
-                '<div class="m-company-listbox1-right">',
-                    '<div class="m-company-listbox2">',
-                        '<div class="listbox-zi">',
-                            '<aa num="${sharecount}"></aa><br />分享',
-                        '</div>',
-                '</div>',
-                    '<div class="m-company-listbox3">',
-                        '<div class="listbox-zi">',
-                            '${consumeractivitycount}<br />超惠活动',
-                        '</div>',
-                    '</div>',
-                '</div>',
-            '</div>',
-            '<div class="m-company-listbox4-w">',
-                '<div class="m-company-listbox4" style="border-left:0;">',
-                    '<div class="listbox-zi">',
-                        '<ab num="${activefanscount}"></ab><br />留存惠粉',
-                    '</div>',
-                '</div>',
-                '<div class="m-company-listbox4">',
-                    '<div class="listbox-zi">',
-                        '<ac num="${fansinhistorycount}"></ac><br />累计惠粉',
-                    '</div>',
-                '</div>',
-                '<div class="m-company-listbox4">',
-                    '<div class="listbox-zi">',
-                        '<ad>${percentageretained}</ad><br />留存',
-                    '</div>',
-                '</div>',
-            '</div>',
-        '</div>'
-        ].join('\n');
-    }
-
+        '</div><div class="title" style="border-bottom:0">公司贡献</div>',
+        '<table cellspacing="5" cellpadding="0" class="company-tbl">',
+        ' <tr><td rowspan="2" class="cbox1"><span>${invitedretailercount}</span><br>邀请店铺</td>',
+        '  <td class="cbox2"><span>${retaileractivitycount}</span><br />店铺活动</td></tr>',
+        '<tr><td> <ul>',
+        '    <li style="border-top-left-radius: 4px;" class="cbox3"><span>${activitycount}</span><br />超惠活动<label class="ico"></label></li>',
+        '   <li style="border-top-right-radius: 4px;" class="cbox4"><span>${activitysharecount}</span><br />分享</li>',
+        '   <li style="border-bottom-left-radius: 4px; border-top: 1px solid #d88738" class="cbox3"><span>${ticketcount}</span><br />超惠券<label class="ico"></label></li>',
+        '    <li style="border-bottom-right-radius: 4px; border-top: 1px solid #d88738" class="cbox4"><span>${ticketsharecount}</span><br />分享</li>',
+        '   </ul></td></tr></table>',
+        '<div class="my-cbox">',
+        '   <div class="f-title"><span dd="dd" num="${activefanscount}">${activefanscount}</span><br />留存惠粉</div>',
+        '   <div class="rt-title"><span bb="bb" num="${fansinhistorycount}">${fansinhistorycount}</span><br />累计拉粉</div>',
+        '   <div class="rn-title"><span cc="cc">${percentageretained}</span><br />留存</div>',
+        '</div>',
+    '</div>'
+    ].join('\n');
+}
 
 suppermarketactivitylist.prototype.render = function () {
-        var container = this.container;
-        var companytemplate = this.companytemplate;
+    var container = this.container;
+    var companytemplate = this.companytemplate;
     $.getJSON2("/webapi/distributor/weixin/distributorstat", function (data) {
-            var html = juicer(companytemplate, data);
-            container.after(html);
-            $("#qrcode").attr("src", $("#qrcode").attr("orgsrc"))
-            var $divWidth = $('.m-company-listbox1').width();
-            $('.m-company-listbox1').css({ 'height': $divWidth * 2 });
+        var html = juicer(companytemplate, data);
+        container.after(html);
+        $("#qrcode").attr("src", $("#qrcode").attr("orgsrc"))
+        var $divWidth = $('.m-company-listbox1').width();
+        $('.m-company-listbox1').css({ 'height': $divWidth * 2 });
 
-            var $divWidth = $('.m-company-listbox1').height();
-            $('.m-company-listbox1-right').css({ 'height': $divWidth });
+        var $divWidth = $('.m-company-listbox1').height();
+        $('.m-company-listbox1-right').css({ 'height': $divWidth });
 
-            var $divWidth = $('.m-company-listbox1').height();
-            $('.m-company-listbox2').css({ 'height': $divWidth / 2 });
+        var $divWidth = $('.m-company-listbox1').height();
+        $('.m-company-listbox2').css({ 'height': $divWidth / 2 });
 
-            var $divWidth = $('.m-company-listbox1').height();
-            $('.m-company-listbox3').css({ 'height': $divWidth / 2 });
+        var $divWidth = $('.m-company-listbox1').height();
+        $('.m-company-listbox3').css({ 'height': $divWidth / 2 });
 
-            var $divWidth = $('.m-company-listbox4').width();
-            $('.m-company-listbox4').css({ 'height': $divWidth });
+        var $divWidth = $('.m-company-listbox4').width();
+        $('.m-company-listbox4').css({ 'height': $divWidth });
 
-            var aa = $('aa').attr('num');
-            var ab = $('ab').attr('num');
-            var ac = $('ac').attr('num');
-            $('aa').text(common.str_pep_num(aa))
-            $('ab').text(common.str_pep_num(ab))
-            $('ac').text(common.str_pep_num(ac))
+        var aa = $('aa').attr('num');
+        var ab = $('ab').attr('num');
+        var ac = $('ac').attr('num');
+        $('aa').text(common.str_pep_num(aa))
+        $('ab').text(common.str_pep_num(ab))
+        $('ac').text(common.str_pep_num(ac))
 
-       
-        });
-    }
+
+    });
+}
 $(function () {
 
     var s = new invitationfans("container");
--- WebPages/distributor/js/my-self-main.js
+++ WebPages/distributor/js/my-self-main.js
@@ -1,68 +1,57 @@
 
-    function employee(containerstat, contaninemployees) {
-        this.containerstat = $(containerstat);
-        this.contaninemployees = $(contaninemployees);
-        this.stattemplate = [
+function employee(containerstat, contaninemployees) {
+    this.containerstat = $(containerstat);
+    this.contaninemployees = $(contaninemployees);
+    this.stattemplate = [
         '<div class="m-self-top">',
-            '<div class="userimg"><img src="{@if employeepic ==""}../image/cs04.jpg {@else}${employeepic}{@/if}" /></div>',
-            '<div class="userinfo">',
-                '<span>${employeename}{@if employeerolename != "" && employeerolename != null}(${employeerolename}){@/if}</span><br />',
-                '${distributorname}',
-            '</div>',
+        '<div class="userimg"><img src="{@if employeepic ==""}../image/cs04.jpg {@else}${employeepic}{@/if}" /></div>',
+        '<div class="userinfo"><span>${employeename}{@if employeerolename != "" && employeerolename != null}(${employeerolename}){@/if}</span><br />${distributorname}</div>',
         '</div>',
-        '<ul class="m-self-listbox">',
-            '<li>',
-                '<span>${invitedretailercount}</span><br />',
-                '邀请店铺',
-            '</li>',
-            '<li>',
-                '<span aa="aa" num="${sharecount}">${sharecount}</span><br />',
-                '分享',
-            '</li>',
-            '<li>',
-                '<span dd="dd" num="${activefanscount}">${activefanscount}</span><br />',
-                '留存惠粉',
-            '</li>',
-            '<li>',
-                '<span bb="bb" num="${fansinhistorycount}">${fansinhistorycount}</span><br />',
-                '累计惠粉',
-            '</li>',
-            '<li>',
-                '<span cc="cc">${percentageretained}</span><br />',
-                '留存',
-            '</li>',
+        '<div class="title" style="border-bottom:0">我的贡献</div> ',
+        '<ul class="my-box">',
+        '<li class="b-bg">',
+        '    <span>${invitedretailercount}</label><br />邀请店铺<br />',
+        ' </li>',
+        ' <li class="o-bg">',
+        '      <span aa="aa" num="${sharecount}">${sharecount}</span><br />分享',
+        '   </li>',
+        '</ul>',
+        '<div class="my-cbox">',
+        '   <div class="f-title"><span dd="dd" num="${activefanscount}">${activefanscount}</span><br />留存惠粉</div>',
+        '   <div class="rt-title"><span bb="bb" num="${fansinhistorycount}">${fansinhistorycount}</span><br />累计拉粉</div>',
+        '   <div class="rn-title"><span cc="cc">${percentageretained}</span><br />留存</div>',
+        '</div>'
+    ].join('\n');
+    this.employeestemplate = [
+        '<div class="title">同事(${employeecount})</div>',
+        '<ul class="ts-list-w">',
+            '{@each employees as item}',
+            '<li><img src="{@if item.employeepic !=""}../image/cs04.jpg {@else}${item.employeepic}{@/if}" />${item.username}（${item.rolename}）</li>',
+            '{@/each}',
         '</ul>'
-        ].join('\n');
-        this.employeestemplate = [
-            '<div class="title">同事(${employeecount})</div>',
-			'<ul class="ts-list-w">',
-				'{@each employees as item}',
-				'<li><img src="{@if item.employeepic !=""}../image/cs04.jpg {@else}${item.employeepic}{@/if}" />${item.username}（${item.rolename}）</li>',
-				'{@/each}',
-			'</ul>'
-        ].join('\n');
-    }
+    ].join('\n');
+}
 
 employee.prototype.render = function () {
-        var stattemplate = this.stattemplate;
-        var employeestemplate = this.employeestemplate;
-        var containerstat = this.containerstat;
-        var contaninemployees = this.contaninemployees;
+    var stattemplate = this.stattemplate;
+    var employeestemplate = this.employeestemplate;
+    var containerstat = this.containerstat;
+    var contaninemployees = this.contaninemployees;
     $.getJSON2("/webapi/distributor/weixin/employeestat", function (data) {
-            var html = juicer(stattemplate, data);
-            containerstat.html(html);
-            var aa = $('[aa=aa]').attr('num');
-            var ab = $('[bb=bb]').attr('num');
-		    var dd = $('[dd=dd]').attr('num');
-            $('[aa=aa]').text(common.str_pep_num(aa));
-            $('[bb=bb]').text(common.str_pep_num(ab));
-		    $('[dd=dd]').text(common.str_pep_num(dd));
-        });
+        var html = juicer(stattemplate, data);
+        containerstat.html(html);
+        var aa = $('[aa=aa]').attr('num');
+        var ab = $('[bb=bb]').attr('num');
+        var dd = $('[dd=dd]').attr('num');
+        $('[aa=aa]').text(common.str_pep_num(aa));
+        $('[bb=bb]').text(common.str_pep_num(ab));
+        $('[dd=dd]').text(common.str_pep_num(dd));
+    });
     $.getJSON2("/webapi/distributor/weixin/employees", function (data) {
-            var html = juicer(employeestemplate, data);
-            contaninemployees.html(html);
-        });
-    }
+        var html = juicer(employeestemplate, data);
+        contaninemployees.html(html);
+    });
+}
 $(function () {
     var s = new invitationfans("container");
     s.render();
